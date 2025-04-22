e290680633640d8e58cc6d6a7ba4b5e22f3d6380
keshew <artyom.korotckov2017@yandex.ru>
2025-04-22 12:39:46 +0500

    Initial Commit

---
+++ GoalMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoalMaster
+//
+//  Created by Артём Коротков on 22.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoalMaster/GoalMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalMasterApp.swift
+//  GoalMaster
+//
+//  Created by Артём Коротков on 22.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct GoalMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
